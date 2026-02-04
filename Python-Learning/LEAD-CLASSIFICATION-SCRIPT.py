c4a60e6924d3ba8cfa60d0a75e9cda8465206e23
Aabis-future11 <aabgeenaabshar@gmail.com>
2026-02-05 00:11:35 +0530

    project1:Lead classification script completed successfully

--- Python-Learning/LEAD-CLASSIFICATION-SCRIPT.py
+++ Python-Learning/LEAD-CLASSIFICATION-SCRIPT.py
@@ -1,40 +1,38 @@
-def classifying_lead(budget, interest):
-    if budget > 0:
-
-        if interest == True: 
-            if budget >= 50000:
-                return " HOT lead with high budget"
-            elif 20000 <= budget < 50000:
-                return " HOT lead with a medium budget"
-            else:
-                return " HOT lead with a low budget"
-            
-        else:
-            if 20000 <= budget <= 50000:
-                return " WARM lead with medium budget"
-            elif budget > 50000: 
-                return " WARM lead with high budget"
-            else:
-                return "COLD lead"
-    else:
+def classifying_lead(budget, interest_level):
+    if budget <= 0:
         return "INVALID budget"
 
-n = int(input("Number of leads: "))
+    if interest_level == "high":
+        if budget >= 50000:
+            return " Hot lead with high budget"
+        elif 20000 <= budget < 50000:
+            return " Hot lead with a medium budget"
+        else:
+            return " Hot lead with a low budget"
+            
+    
+    elif interest_level == "medium":
+        if budget >= 20000:
+            return " Warm lead with good budget"
+        else:
+            return " Warm lead with low budget"
+        
+    else:
+        return "Cold lead"
+
+n = int(input("Number of leads:"))
 
 for i in range(n):
     print(f"\n--- Entering Data for Lead #{i+1} ---")
     name = input("Enter your name: ")
     budget1 = int(input("Enter your budget: "))
-    interest1 = input("Enter your interest (high/low/medium): ").lower()
-
-
-    is_interested = (interest1 == "high")
+    interest1 = input("Enter your interest (high/medium/low): ").lower()
     
-    status = classifying_lead(budget1, is_interested)
+    status = classifying_lead(budget1, interest1)
     
-    print(f"\n#------- LEAD SUMMARY -------#")
+    print(f"#------- LEAD SUMMARY -------#")
     print(f"Name: {name}")
-    print(f"Details: {budget1}, Interest: {interest1}")
+    print(f"Details: Budget {budget1}, Interest: {interest1}")
     print(f"Classification: {status}")
     print()
 
